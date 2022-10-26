068a95352b9d976f4c1cdd4e43d8772467db261c
johngjackson0 <johngjackson0@gmail.com>
2022-10-26 19:12:46 -0400

    feat: add tasks

---
+++ lib/models/task.dart
@@ -0,0 +1,63 @@
+import 'package:equatable/equatable.dart';
+
+/* 
+  Person("Bob") == Person("Bob") is normally false
+  override == and hashCode to compare objects 
+  by value or extend equatable, requires final properties
+*/
+
+class Task extends Equatable {
+  final String title;
+  final bool? isDone;
+  final bool? isDeleted;
+
+  Task({required this.title, required this.isDone, required this.isDeleted}) {
+    // ??   return isDone if null then false
+    // isDone = isDone ?? false;
+    // isDeleted = isDeleted ?? false;
+  }
+
+  // recreate obj instead of mutate
+  Task copyWith({
+    String? title,
+    bool? isDone,
+    bool? isDeleted,
+  }) {
+    return Task(
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+      isDeleted: isDeleted ?? this.isDeleted,
+    );
+  }
+
+  /* 
+    point:
+    transfers non-primitive of obj to map as 
+    it can be serialized, Map<String, dynamic> =
+    {String: anything, ... }
+  */
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'isDone': isDone,
+      'isDeleted': isDeleted,
+    };
+  }
+
+  /*
+    factory type has return but never uses 'this' :
+    turns map back into instantiated class, therefor
+    alows us to save objects and re-hydrate
+  */
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      title: map['title'] as String,
+      isDone: map['isDone'] != null ? map['isDone'] as bool : null,
+      isDeleted: map['isDeleted'] != null ? map['isDeleted'] as bool : null,
+    );
+  }
+
+  @override
+  List<Object?> get props => [title, isDone, isDeleted];
+}
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import '../models/task.dart';
 
 class TasksScreen extends StatelessWidget {
-  const TasksScreen({Key? key}) : super(key: key);
+  TasksScreen({Key? key}) : super(key: key);
+
+  final List<Task> tasksList = [
+    Task(title: 'Task1', isDeleted: false, isDone: false),
+    Task(title: 'Task2', isDeleted: false, isDone: false),
+    Task(title: 'Task3', isDeleted: false, isDone: false)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +24,26 @@ class TasksScreen extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Center(
+        children: [
+          const Center(
             child: Chip(
               label: Text(
                 'Tasks:',
               ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasksList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(tasksList[index].title),
+                  trailing: Checkbox(
+                    value: tasksList[index].isDone,
+                    onChanged: (value) {},
+                  ),
+                );
+              },
             ),
           ),
         ],
