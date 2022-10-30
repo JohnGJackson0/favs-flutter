bcfd16300d387cb468f0c56ff08ad82909003e39
johngjackson0 <johngjackson0@gmail.com>
2022-10-30 12:00:59 -0400

    feat: add correct task number in drawer

--- lib/screens/my_drawer.dart
+++ lib/screens/my_drawer.dart
@@ -2,6 +2,8 @@ import 'package:favs_app/screens/recycle_bin.dart';
 import 'package:favs_app/screens/tasks_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../blocs/bloc_exports.dart';
+
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
@@ -16,12 +18,16 @@ class MyDrawer extends StatelessWidget {
                 child: const Text('Task Drawer'),
                 padding:
                     const EdgeInsets.symmetric(vertical: 14, horizontal: 20)),
-            GestureDetector(
-              onTap: () => Navigator.of(context).pushNamed(TasksScreen.id),
-              child: const ListTile(
-                  leading: Icon(Icons.folder_special),
-                  title: Text('My Tasks'),
-                  trailing: Text('0')),
+            BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
+                return GestureDetector(
+                  onTap: () => Navigator.of(context).pushNamed(TasksScreen.id),
+                  child: ListTile(
+                      leading: const Icon(Icons.folder_special),
+                      title: const Text('My Tasks'),
+                      trailing: Text('${state.allTasks.length}')),
+                );
+              },
             ),
             const Divider(),
             GestureDetector(
